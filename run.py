7b50d48b1222a55fa05d57cd255c23378d958500
CreatedBySarika <sahsarika2005@gmail.com>
2026-09-11 21:09:30 +0100

    match case

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
 import bot
+import database as db
 
-user = input("Enter the Username: ")
-pwd = input("Enter the password:")
-url = input("Enter the URL to the login page :")
+option = int(input("Welcome to the the login bot press the following number:-\n[1]login\n[2]guest user\n[3]Create User\n"))
 
-bot.startBot(user,pwd,url)
+match option:
+    case 1:
+        us = input("Enter you username")
+        db.check_user(us)
+    case 2:
+        user = input("Enter the Username: ")
+        pwd = input("Enter the password:")
+        url = input("Enter the URL to the login page :")
+        bot.startBot(user,pwd,url)
+    case 3:
+        u = input ("Enter username: ")
+        p = input("Enter password: ")
+        db.create_user(u,p)
+
